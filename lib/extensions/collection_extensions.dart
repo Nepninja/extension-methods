14076da0ff96455ef04359c6bec081b1c8ab3cea
rajesh <rajesh.k.khadka@gmail.com>
2020-08-21 08:21:36 +0545

    add validation extension method

--- lib/extensions/collection_extensions.dart
+++ lib/extensions/collection_extensions.dart
@@ -41,4 +41,27 @@ extension StringExtension on String {
             "${e.length > 1 ? e.substring(1).toLowerCase() : ""}")
         .join(" ");
   }
+
+  bool isEmail() {
+    return RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(this);
+  }
+
+  /*
+      https://stackoverflow.com/questions/5142103/regex-to-validate-password-strength
+
+       ^                         Start anchor
+      (?=.*[A-Z].*[A-Z])        Ensure string has two uppercase letters.
+      (?=.*[!@#$&*])            Ensure string has one special case letter.
+      (?=.*[0-9].*[0-9])        Ensure string has two digits.
+      (?=.*[a-z].*[a-z].*[a-z]) Ensure string has three lowercase letters.
+      .{8}                      Ensure string is of length 8.
+      $                         End anchor.
+   */
+  bool isValidPassword() {
+    return RegExp(
+            r"^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$")
+        .hasMatch(this);
+  }
 }
